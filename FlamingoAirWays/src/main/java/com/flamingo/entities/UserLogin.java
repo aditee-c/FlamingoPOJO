e677cd5d0ed1196fcc1337220c52f4b9b8a4d15f
aditee-c <55917443+aditee-c@users.noreply.github.com>
2019-10-04 19:37:53 +0530

    Add files via upload

---
+++ FlamingoAirWays/src/main/java/com/flamingo/entities/UserLogin.java
@@ -0,0 +1,56 @@
+package com.flamingo.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class UserLogin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@OneToOne
+	@JoinColumn(name = "profileid")
+	private Passenger profileid;
+	@Column(nullable = false)
+	private String username;
+	@Column(nullable = false)
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Passenger getProfileId() {
+		return profileid;
+	}
+
+	public void setProfileId(Passenger profileid) {
+		this.profileid = profileid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ FlamingoAirWays/src/main/java/com/flamingo/services/FlightServicesImpl.java
@@ -0,0 +1,87 @@
+package com.flamingo.services;
+import java.time.LocalDate;
+import java.util.ArrayList;
+/**
+ * @author Aditee Chourasiya
+ * @creation_date  4 Oct 2019 11.50AM
+ * @modification_date 4 Oct 2019 11.50AM
+ * @version 1.0
+ * @copyright Zensar Technologies. All rights resesrved
+ * @description It is a Service class Implementation of Flight used in Bussiness layer
+ */
+import java.util.List;
+
+import com.flamingo.dao.FlightDao;
+import com.flamingo.dao.FlightDaoImpl;
+import com.flamingo.entities.Flight;
+
+
+
+public class FlightServicesImpl implements FlightServices {
+	private FlightDao flightDao;
+	public FlightServicesImpl() {
+		flightDao=new FlightDaoImpl();
+	}
+	@Override
+	public List<Flight> findAllFlights() {
+		
+		return flightDao.getAll();
+	}
+
+	@Override
+	public Flight findFlightById(int flightid) {
+		
+		return flightDao.getById(flightid);
+	}
+
+	@Override
+	public void addFlight(Flight flight) {
+		flightDao.insert(flight);
+
+	}
+
+	@Override
+	public void updateFlight(Flight flight) {
+		flightDao.update(flight);
+
+	}
+
+	@Override
+	public void removeFlight(Flight flight) {
+		
+		flightDao.delete(flight);
+	}
+	
+	@Override
+	public List<Flight> findFlightByLocation(String from, String to) {
+				List<Flight> all= flightDao.getAll();
+				List<Flight> filterByLocation = new ArrayList<>();
+				for (Flight flight : all) {
+					if(flight.getFromLocation()==from && flight.getToLocation()==to) {
+						filterByLocation.add(flight);
+					}
+				}
+				return filterByLocation;
+	}
+	@Override
+	public List<Flight> findFlightByDate(LocalDate date) {
+				List<Flight> all= flightDao.getAll();
+				List<Flight> filterByDate = new ArrayList<>();
+				for (Flight flight : all) {
+					if(flight.getDepartureDate()==date) {
+						filterByDate.add(flight);
+					}
+				}
+				return filterByDate;
+	}
+	@Override
+	public Long getCountOfSeats(){
+	
+	long count=0;
+	List<Flight> all = flightDao.getAll();
+	for (Flight flight : all) {
+		count++;
+	}
+	return  count;
+	}
+}
